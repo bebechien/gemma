69b869197525b1b040375cfe4e864eef9c49ab5f
Juyeong Ji <bebechien@gmail.com>
2024-04-16 17:52:00 +0900

    Update santa.cc

--- santa.cc
+++ santa.cc
@@ -37,46 +37,6 @@
 #include "hwy/profiler.h"
 #include "hwy/timer.h"
 
-darkchien@bonobono:~/gemma.cpp$ more simple.cc 
-// Copyright 2024 Google LLC
-// SPDX-License-Identifier: Apache-2.0
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     https://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-// Command line text interface to gemma.
-
-#include <ctime>
-#include <iostream>
-#include <random>
-#include <string>
-#include <thread>  // NOLINT
-#include <vector>
-
-// copybara:import_next_line:gemma_cpp
-#include "compression/compress.h"
-// copybara:import_next_line:gemma_cpp
-#include "gemma.h"    // Gemma
-// copybara:import_next_line:gemma_cpp
-#include "util/app.h"
-// copybara:import_next_line:gemma_cpp
-#include "util/args.h"  // HasHelp
-#include "hwy/base.h"
-#include "hwy/contrib/thread_pool/thread_pool.h"
-#include "hwy/highway.h"
-#include "hwy/per_target.h"
-#include "hwy/profiler.h"
-#include "hwy/timer.h"
-
 namespace gcpp {
 
 void RunSimple(LoaderArgs& loader, InferenceArgs& inference, AppArgs& app, std::string prompt_string) {
